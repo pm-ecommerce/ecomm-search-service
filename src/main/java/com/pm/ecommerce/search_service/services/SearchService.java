2d217ffce2cb38046e5827c908e04f3ddc7299bb
Sandeep Giri <ioesandeep@gmail.com>
2020-10-24 21:36:44 -0400

    Filter request

--- src/main/java/com/pm/ecommerce/search_service/services/SearchService.java
+++ src/main/java/com/pm/ecommerce/search_service/services/SearchService.java
@@ -18,18 +18,18 @@ public class SearchService {
     public List<ProductResult> getProductsByFilter(FilterRequest request) {
         Integer offset = (request.getPage() - 1) * request.getLimit();
 
-        if (request.getQuery().equals("") && request.getCategoryId() == 0) {
+        if (request.getName().equals("") && request.getCategoryId() == 0) {
             return searchRepository.getProductsByFilterNoNameAndCategory(request.getHighPrice(), request.getLowPrice(), request.getLimit(), offset).stream().map(ProductResult::new).collect(Collectors.toList());
         }
 
-        if (request.getQuery().equals("")) {
+        if (request.getName().equals("")) {
             return searchRepository.getProductsByFilterNoName(request.getCategoryId(), request.getHighPrice(), request.getLowPrice(), request.getLimit(), offset).stream().map(ProductResult::new).collect(Collectors.toList());
         }
 
         if (request.getCategoryId() == 0) {
-            return searchRepository.getProductsByFilterNoCategory(request.getQuery(), request.getHighPrice(), request.getLowPrice(), request.getLimit(), offset).stream().map(ProductResult::new).collect(Collectors.toList());
+            return searchRepository.getProductsByFilterNoCategory(request.getName(), request.getHighPrice(), request.getLowPrice(), request.getLimit(), offset).stream().map(ProductResult::new).collect(Collectors.toList());
         }
 
-        return searchRepository.getProductsByFilter(request.getQuery(), request.getCategoryId(), request.getHighPrice(), request.getLowPrice(), request.getLimit(), offset).stream().map(ProductResult::new).collect(Collectors.toList());
+        return searchRepository.getProductsByFilter(request.getName(), request.getCategoryId(), request.getHighPrice(), request.getLowPrice(), request.getLimit(), offset).stream().map(ProductResult::new).collect(Collectors.toList());
     }
 }
