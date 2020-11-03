a9b7cc1b7f6a35f67593b08af42ee69aedd7dbe6
zayed016 <zayedemails@gmail.com>
2020-11-03 17:17:24 -0500

    sort by id desc

--- src/main/java/com/pm/ecommerce/search_service/repositories/CategoryRepository.java
+++ src/main/java/com/pm/ecommerce/search_service/repositories/CategoryRepository.java
@@ -18,7 +18,7 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     @Query(value="Select distinct c from Category c Join Product p on p.category.id=c.id Join Vendor v on p.vendor.id=v.id where p.status=:productStatus and v.status=:vendorStatus and c.isDeleted=false and c.parent IS NULL ORDER BY c.name")
     List<Category> getAllCategories(ProductStatus productStatus, VendorStatus vendorStatus);
 
-    @Query(value="Select p from Product p where p.category.id =:categoryId and p.status=:productStatus ORDER BY p.name" )
+    @Query(value="Select p from Product p where p.category.id =:categoryId and p.status=:productStatus ORDER BY p.id DESC" )
     Page<Product> getProductsByCategoryId(Integer categoryId, ProductStatus productStatus, Pageable page);
 
     @Query(value="select distinct c.* from categories c Join products p on p.category_id=c.id Join vendors v on p.vendor_id=v.id where p.status=4 and v.status=3 and c.is_deleted=0 order by rand() limit 4;",  nativeQuery = true)
